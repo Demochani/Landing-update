dcf6771491c9b56b55ad58972cd928ff6748f652
Nikita <dn200501@gmail.com>
2022-09-01 17:55:23 +0300

    it still doesnt work...

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import SearchItem from "../SearchItem";
 import { usePosts } from "../hooks/usePosts";
+import Context from "../hooks/context/Context";
+import axios from "axios";
+
 const Search = () => {
+  const { postQuery } = useContext(Context);
   const [posts, setPosts] = useState([]);
-  const searchedPosts = usePosts(posts, postQuery);
+  // const searchedPosts = usePosts(posts, postQuery);
 
-
+useEffect(() =>  {
+    const res =  axios.get('https://jsonplaceholder.typicode.com/photos?_limit=100');
+    setPosts(posts);
+    
+}, [])
   return (
     <div className="search-posts-container">
       <hr style={{ margin: "20px 0 15px 0", color: "#C0C0C0" }} />
@@ -14,9 +22,9 @@ const Search = () => {
       </strong>
       <div className="search-posts">
         {posts
-          // .filter(
-          //   (post) => post.title.toLowerCase().includes(postQuery) //
-          // )
+          .filter(
+            (post) => post.title.toLowerCase().includes(postQuery) //
+          )
           .map((post, index) => (
             <SearchItem number={index + 1} post={post} key={post.id} />
           ))}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import Context from "./components/hooks/context/Context";
+import postQuery from './components/pages/Posts'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Context.Provider value={{postQuery}}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Context.Provider>
   </React.StrictMode>
 );
