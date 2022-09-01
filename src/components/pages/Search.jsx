9e97b490e3fa7f4226f98d3ea1090bd68ca69fc9
Nikita <dn200501@gmail.com>
2022-09-01 17:20:53 +0300

    added Context

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -1,20 +1,25 @@
-import React from "react";
-import NavBar from "../NavBar";
-import PostItem from "../PostItem";
-
+import React, { useState } from "react";
+import SearchItem from "../SearchItem";
+import { usePosts } from "../hooks/usePosts";
 const Search = () => {
+  const [posts, setPosts] = useState([]);
+  const searchedPosts = usePosts(posts, postQuery);
+
+
   return (
     <div className="search-posts-container">
       <hr style={{ margin: "20px 0 15px 0", color: "#C0C0C0" }} />
       <strong style={{ margin: "10px 0 0 4%", color: "#C0C0C0" }}>
-        INTERESTING
+        SEARCH
       </strong>
       <div className="search-posts">
-        {posts.filter(
-          post => post.title.toLowerCase().includes(postQuery)//
-        ).map((post, index) => (
-          <PostItem number={index + 1} post={post} key={post.id} />
-        ))}
+        {posts
+          // .filter(
+          //   (post) => post.title.toLowerCase().includes(postQuery) //
+          // )
+          .map((post, index) => (
+            <SearchItem number={index + 1} post={post} key={post.id} />
+          ))}
       </div>
     </div>
   );
